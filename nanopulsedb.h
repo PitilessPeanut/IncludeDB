c8062b4526f40801a18b3a8c52c2283abb421e88
Stephen-van-Helsing <stephen-van-helsing.bandwork@slmail.me>
2020-08-04 19:12:30 +0800

    stuff..

--- nanopulsedb.h
+++ nanopulsedb.h
@@ -10,6 +10,9 @@
   LICENSE
       See end of file.
  
+  HISTORY
+      0.1.0  Initial release
+ 
   DONATE
       This project cannot continue without YOU! Please support! It is MUCH appreciated!
         BTC:  1H1RrCrEgUXDFibpaJciLjS9r7upQs6XPc
@@ -32,36 +35,37 @@
 
 #if defined(COMP_AS_CPP)
   #define COMPTIME constexpr
-  #define CTOR4(w,x,y,z) constexpr w,x,y,z
+  #define CTOR3(x,y,z) constexpr x,y,z
 #else
   #define COMPTIME static const
   #define constexpr
   #define nullptr NULL
   enum {false=0,true=1};
   typedef int bool;
-  #define CTOR4(w,x,y,z)
+  #define CTOR3(x,y,z)
 #endif
 
 
-struct nplse_bitvec;
+struct nplse__bitvec;
 
-typedef unsigned (*pnplse_bitvec_alloc)(struct nplse_bitvec *bitvec, int amount);
+typedef unsigned (*pnplse__bitvecAlloc)(struct nplse__bitvec *bitvec, int amount);
 
-typedef struct nplse_bitvec
+typedef struct nplse__bitvec
 {
+    CTOR3(nplse__bitvec() : bitvec(nullptr), size(0), nplse__bitvecAlloc(nullptr) {} )
     unsigned *bitvec;
     int size;
-    pnplse_bitvec_alloc nplse_bitvec_alloc;
-} nplse_bitvec;
+    pnplse__bitvecAlloc nplse__bitvecAlloc;
+} nplse__bitvec;
 
 
-typedef struct nplse_skipnode
+typedef struct nplse__skipnode
 {
-    CTOR4(nplse_skipnode() : nodeid(0), filepos(0), prev(0), next(0) {})
+    CTOR3(nplse__skipnode() : nodeid(0), filepos(0), next(0) {})
     unsigned nodeid;
     int filepos;
-    int prev, next; // offsets
-} nplse_skipnode;
+    int next; // offset
+} nplse__skipnode;
 
 /*
  1. ''priority' list
@@ -82,12 +86,12 @@ typedef struct nanopulseDB
 {
     // File:
     unsigned char *mappedfile;
-    nplse_bitvec occupied;
+    nplse__bitvec occupied;
     int szMappedfile;
     int chunkSize;
     
     // List:
-    nplse_skipnode *nodeVec;
+    nplse__skipnode *nodeVec;
     int rootA=0, rootB=0, rootC=0, rootD=0;
     int nKeys = 0;
     int nAllocatedSlots = 32;
@@ -101,7 +105,7 @@ typedef struct nanopulseDB
 
 
 // Public interface
-//static nplse *nplse_open(nplse_internal_test *test);
+//static nplse *nplse_open(*test);
 //static void nplse_close(nplse *instance);
 
 
@@ -109,11 +113,9 @@ typedef struct nanopulseDB
 
 #ifdef NANOPULSE_DB_IMPLEMENTATION
 
-static constexpr unsigned nplse_xx32(const unsigned char *input, int len, unsigned seed)
+static constexpr unsigned nplse__xx32(const unsigned char *input, int len, unsigned seed)
 {
     // https://github.com/Cyan4973/xxHash
-    
-    #define XXH_rotl32(x,r) (((x) << (r)) | ((x) >> (32 - (r))))
     COMPTIME unsigned XXH_PRIME32_1 = 0x9E3779B1u;
     COMPTIME unsigned XXH_PRIME32_2 = 0x85EBCA77u;
     COMPTIME unsigned XXH_PRIME32_3 = 0xC2B2AE3Du;
@@ -121,6 +123,7 @@ static constexpr unsigned nplse_xx32(const unsigned char *input, int len, unsign
     COMPTIME unsigned XXH_PRIME32_5 = 0x165667B1u;
     const unsigned char *bEnd = input+len;
     unsigned h=0, bytes=0;
+    #define XXH_rotl32(x,r) (((x) << (r)) | ((x) >> (32 - (r))))
     if (len>=16)
     {
         const unsigned char *const limit = bEnd - 15;
@@ -130,22 +133,22 @@ static constexpr unsigned nplse_xx32(const unsigned char *input, int len, unsign
         unsigned v4 = seed - XXH_PRIME32_1;
         do
         {
-            bytes = (unsigned)input[0] | ((unsigned)input[1]<< 8) | ((unsigned)input[2]<<16) | ((unsigned)input[3]<<24);
+            bytes = (input[0]+0u) | ((input[1]+0u)<< 8) | ((input[2]+0u)<<16) | ((input[3]+0u)<<24);
             v1 += bytes * XXH_PRIME32_2;
             v1  = XXH_rotl32(v1, 13);
             v1 *= XXH_PRIME32_1;
             input += 4;
-            bytes = (unsigned)input[0] | ((unsigned)input[1]<< 8) | ((unsigned)input[2]<<16) | ((unsigned)input[3]<<24);
+            bytes = (input[0]+0u) | ((input[1]+0u)<< 8) | ((input[2]+0u)<<16) | ((input[3]+0u)<<24);
             v2 += bytes * XXH_PRIME32_2;
             v2  = XXH_rotl32(v2, 13);
             v2 *= XXH_PRIME32_1;
             input += 4;
-            bytes = (unsigned)input[0] | ((unsigned)input[1]<< 8) | ((unsigned)input[2]<<16) | ((unsigned)input[3]<<24);
+            bytes = (input[0]+0u) | ((input[1]+0u)<< 8) | ((input[2]+0u)<<16) | ((input[3]+0u)<<24);
             v3 += bytes * XXH_PRIME32_2;
             v3  = XXH_rotl32(v3, 13);
             v3 *= XXH_PRIME32_1;
             input += 4;
-            bytes = (unsigned)input[0] | ((unsigned)input[1]<< 8) | ((unsigned)input[2]<<16) | ((unsigned)input[3]<<24);
+            bytes = (input[0]+0u) | ((input[1]+0u)<< 8) | ((input[2]+0u)<<16) | ((input[3]+0u)<<24);
             v4 += bytes * XXH_PRIME32_2;
             v4  = XXH_rotl32(v4, 13);
             v4 *= XXH_PRIME32_1;
@@ -162,7 +165,7 @@ static constexpr unsigned nplse_xx32(const unsigned char *input, int len, unsign
     len &= 15;
     while (len >= 4)
     {
-        bytes = (unsigned)input[0] | ((unsigned)input[1]<< 8) | ((unsigned)input[2]<<16) | ((unsigned)input[3]<<24);
+        bytes = (input[0]+0u) | ((input[1]+0u)<< 8) | ((input[2]+0u)<<16) | ((input[3]+0u)<<24);
         h += bytes * XXH_PRIME32_3;
         input += 4;
         h  = XXH_rotl32(h, 17) * XXH_PRIME32_4;
@@ -183,9 +186,9 @@ static constexpr unsigned nplse_xx32(const unsigned char *input, int len, unsign
     return h;
 }
 
-inline constexpr void nplse_insertSkipnode(nanopulseDB *instance, unsigned key, int filepos)
+inline constexpr void nplse__insertSkipnode(nanopulseDB *instance, unsigned key, int filepos)
 {
-    const int newNodeAddr = nplse_internal_getNewNodePos(instance);
+    const int newNodeAddr = nplse__getNewNodePos(instance);
     instance->nodeVec[newNodeAddr].nodeid  = key;
     instance->nodeVec[newNodeAddr].filepos = filepos;
     
@@ -212,13 +215,19 @@ inline constexpr void nplse_insertSkipnode(nanopulseDB *instance, unsigned key, 
     instance->nodeVec[current].next = newNodeAddr;
 }
 
-inline constexpr nplse_skipnode *nplse_findSkipnode(nanopulseDB *instance, const unsigned key)
+inline constexpr nplse__skipnode *nplse__findSkipnode(nanopulseDB *instance, const unsigned key, int *prev)
 {
     int current = instance->headD;
+    int p = current;
     for (int i=0; i<instance->nKeys; ++i)
     {
         if (instance->nodeVec[current].nodeid == key)
+        {
+            if (prev)
+                *prev = p;
             return &instance->nodeVec[current];
+        }
+        p = current;
         current = instance->nodeVec[current].next;
     }
     return nullptr;
@@ -229,10 +238,10 @@ inline constexpr nplse_skipnode *nplse_findSkipnode(nanopulseDB *instance, const
 
 #if defined(COMP_AS_CPP)
 
-constexpr unsigned nplse_testSkiplist()
+constexpr unsigned nplse__testSkiplist()
 {
     // setup:
-    nplse_skipnode testNodes[32];
+    nplse__skipnode testNodes[32];
     nanopulseDB testDB{ .mappedfile=nullptr,
                         .szMappedfile=0,
                         .chunkSize=0,
@@ -240,9 +249,9 @@ constexpr unsigned nplse_testSkiplist()
                         .seed=0
                       };
     // start testing:
-    nplse_insertSkipnode(&testDB, 111, 0);
+    nplse__insertSkipnode(&testDB, 111, 0);
     const bool TEST_HEAD_IS_ZERO = testDB.headD == 0;
-    nplse_insertSkipnode(&testDB, 333, 1);
+    nplse__insertSkipnode(&testDB, 333, 1);
     const bool TEST_NODE_ADD =  testNodes[0].nodeid == 111
                              && testNodes[1].nodeid == 333
                              && testNodes[testDB.headD].nodeid == 111
@@ -253,26 +262,27 @@ constexpr unsigned nplse_testSkiplist()
     const bool TEST_NODES_POINTING_TO_CORRECT_POSITION =  testNodes[0].filepos == 0
                                                        && testNodes[1].filepos == 1;
     // insert node between:
-    nplse_insertSkipnode(&testDB, 222, 2);
+    nplse__insertSkipnode(&testDB, 222, 2);
     const bool TEST_NODE_BETWEEN =  testNodes[2].nodeid == 222
                                  && testNodes[2].next == 1
                                  && testNodes[1].next == 0
                                  && testNodes[0].next == 2;
     // find nodes:
-    const nplse_skipnode *first  = nplse_findSkipnode(&testDB, 111);
-    const nplse_skipnode *second = nplse_findSkipnode(&testDB, 333);
-    const nplse_skipnode *third  = nplse_findSkipnode(&testDB, 222);
-    const bool TEST_NODES_FOUND =  first->filepos == 0
-                                && second->filepos == 1
-                                && third->filepos == 2;
+    int prevFirst=0, prevSecond=0, prevThird=0;
+    const nplse__skipnode *first  = nplse__findSkipnode(&testDB, 111, &prevFirst);
+    const nplse__skipnode *second = nplse__findSkipnode(&testDB, 333, &prevSecond);
+    const nplse__skipnode *third  = nplse__findSkipnode(&testDB, 222, &prevThird);
+    const bool TEST_NODES_FOUND =  first->filepos==0  && prevFirst==0
+                                && second->filepos==1 && testNodes[prevSecond].nodeid==222
+                                && third->filepos==2  && testNodes[prevThird].nodeid==111;
     // add node to end:
-    nplse_insertSkipnode(&testDB, 444, 3);
-    const bool TEST_NODE_AT_END =  nplse_findSkipnode(&testDB, 444)->filepos == 3
+    nplse__insertSkipnode(&testDB, 444, 3);
+    const bool TEST_NODE_AT_END =  nplse__findSkipnode(&testDB, 444, nullptr)->filepos == 3
                                 && testNodes[3].nodeid == 444
                                 && testNodes[1].next == 3
                                 && testNodes[3].next == 0;
     // not found:
-    const bool TEST_NODE_NOT_FOUND = nplse_findSkipnode(&testDB, 69) == nullptr;
+    const bool TEST_NODE_NOT_FOUND = nplse__findSkipnode(&testDB, 69, nullptr) == nullptr;
     
     
     return  TEST_HEAD_IS_ZERO
@@ -285,7 +295,7 @@ constexpr unsigned nplse_testSkiplist()
          ;
 }
 
-static constexpr unsigned resList = nplse_testSkiplist();
+static constexpr unsigned resList = nplse__testSkiplist();
 
 static_assert(resList&  1, "head pointing to wrong place");
 static_assert(resList&  2, "node was not correctly inserted");
@@ -293,10 +303,10 @@ static_assert(resList&  4, "nodes did not sit where they were supposed to");
 static_assert(resList&  8, "nodes not correctly sorted");
 static_assert(resList& 16, "nodes could not be recovered");
 static_assert(resList& 32, "node at end not found");
-static_assert(resList& 64, "nplse_findSkipnode() did not return NULL");
+static_assert(resList& 64, "nplse__findSkipnode() did not return NULL");
 
 
-constexpr unsigned nplse_testDB()
+constexpr unsigned nplse__testDB()
 {
 }
 
