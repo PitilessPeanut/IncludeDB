ea8e6f6a9592f8474583fb6d19f792ee5a68c323
Professor-Peanut <professorpeanut.unpaid@simplelogin.fr>
2020-09-05 18:56:28 +0800

    examples

--- demo_nanopulsedb.c
+++ demo_nanopulsedb.c
@@ -6,7 +6,23 @@ int main(void)
     // Create new file or open existing:
     nanopulseDB *db = nplse_open("hello.dat");
     if (!db)
+    {
         printf("error opening db\n");
+    }
     
+    
+    // Iterate over all keys:
+    int keylen;
+    unsigned char *curKey = nplse_curGet(db, &keylen);
+    while (curKey)
+    {
+    
+    
+        // Advance cursor:
+        nplse_next(db);
+    }
+    
+    
+    // Done:
     nplse_close(db);
 }
